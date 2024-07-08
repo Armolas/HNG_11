3626b3a528f594d5b4dc67727455a3c55f2aac57
Armolas <armolas06@gmail.com>
2024-07-08 13:12:23 +0100

    vercel

--- stage_2/user_auth/settings.py
+++ stage_2/user_auth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-woggg!=j+4m2g33vg%t4k!3&1c==kdw*efu$22s7p%785_riaw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['arm-auth.onrender.com']
+ALLOWED_HOSTS = ['arm-auth.onrender.com', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
